877204dbf041efa0621ace878826617d511200e3
Emmanuel-254 <emmanueltikan@gmail.com>
2024-10-06 23:58:22 +0300

    Refactor HTML and CSS formatting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+	const taskForm = document.getElementById('create-task-form');
+	const taskList = document.getElementById('tasks');
+
+	taskForm.addEventListener('submit', function (event) {
+		event.preventDefault();
+
+		const newTaskDescription = document.getElementById(
+			'new-task-description'
+		).value;
+
+		if (newTaskDescription.trim() !== '') {
+			const taskItem = document.createElement('li');
+			taskItem.textContent = newTaskDescription;
+
+			const deleteButton = document.createElement('button');
+			deleteButton.textContent = 'Delete';
+			deleteButton.style.marginLeft = '10px';
+			deleteButton.addEventListener('click', function () {
+				taskItem.remove();
+			});
+
+			taskItem.appendChild(deleteButton);
+
+			taskList.appendChild(taskItem);
+
+			taskForm.reset();
+		}
+	});
 });
